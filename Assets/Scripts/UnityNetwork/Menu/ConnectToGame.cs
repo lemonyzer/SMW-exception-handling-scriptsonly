f8bcee17b723d19a2f122dde33432abb8b579904
Lemon Nyzer <lemonyzer@gmail.com>
2014-09-18 15:34:31 +0200

    "SuperMarioWars 2014.09.18_3_UnityNetwork_workingOnNAT-Traversal_verify3.rar"

--- Assets/Scripts/UnityNetwork/Menu/ConnectToGame.cs
+++ Assets/Scripts/UnityNetwork/Menu/ConnectToGame.cs
@@ -4,6 +4,21 @@ using System.Collections.Generic;
 
 public class ConnectToGame : MonoBehaviour
 {
+	public string registeredGameName = "smw_alpha";
+	public string registeredGameType = "platformer";
+	public string registeredGameComment = "classic";
+
+	public string testStatus = "Testing network connection capabilities.";
+	public string testMessage = "Test in progress";
+	public string shouldEnableNatMessage;
+	public bool doneTesting = false;
+	public bool probingPublicIP = false;
+	private int serverPort = 25005;
+	public ConnectionTesterStatus connectionTestResult = ConnectionTesterStatus.Undetermined;
+	public float timer = 0;
+	// Indicates if the useNat parameter be enabled when starting a server
+	public bool useNat = false;
+
 	public int clientSlots = 10;
 
 	private string ip = "tofast.ddns.net";
@@ -27,6 +42,8 @@ public class ConnectToGame : MonoBehaviour
 		}
 	}
 
+	NatTest natTest;
+
 	void Awake()
 	{
 		hostList = new List<Host>();
@@ -35,6 +52,10 @@ public class ConnectToGame : MonoBehaviour
 		hostList.Add(new Host("MBlack", ipMBlack));
 
 		InitGUIStyle ();
+
+		//UPnPTest();
+		natTest = new NatTest();
+		natTest.Start();
 	}
 
 	/**
@@ -58,11 +79,35 @@ public class ConnectToGame : MonoBehaviour
 		}
 	}
 
+	public string upnpStatus = "Test not started";
+
+	void UPnPTest()
+	{
+		upnpStatus = "Test running";
+        if(NAT.DiscoverWithTryCatch())
+		{
+			upnpStatus = "You have an UPnP-enabled router and your IP is: "+NAT.GetExternalIP();
+		}
+		else
+			upnpStatus = "You do not have an UPnP-enabled router.";
+    }
+
 	void OnGUI()
 	{
-		if(Network.peerType == NetworkPeerType.Client ||
-		   Network.peerType == NetworkPeerType.Server)
-			return;
+//		if(Network.peerType == NetworkPeerType.Client ||
+//		   Network.peerType == NetworkPeerType.Server)
+//			return;
+
+		GUILayout.Label("UPnP Status: " + upnpStatus);
+
+		GUILayout.Label("Current Status: " + testStatus);
+		GUILayout.Label("Test result : " + testMessage);
+		GUILayout.Label(shouldEnableNatMessage);
+		if (!doneTesting)
+		{
+			TestConnection();
+//			return;
+		}
 
 		// let the user enter IP address
 		GUILayout.Label( "IP Address" );
@@ -85,10 +130,27 @@ public class ConnectToGame : MonoBehaviour
 		GUILayout.EndHorizontal();
 
 		// host a server on the given port, only allow 3 incoming connection (3 other players)
-		if( GUILayout.Button( "Host", GUILayout.Width( 100f ), GUILayout.MinHeight(minButtonHeight) ) )
+		if( GUILayout.Button( "Host offline", GUILayout.Width( 100f ), GUILayout.MinHeight(minButtonHeight) ) )
 		{
-			Network.InitializeServer( clientSlots, port, true );
+			Network.InitializeServer( clientSlots, port, useNat );
+			
+			//			You can call RegisterHost more than once while a server is running 
+			//			to update the information stored on the Master Server. For example, 
+			//			if the server changes to a new level, you might call RegisterHost 
+			//			again to update the lobby.
 		}
+		GUI.enabled = doneTesting;
+		if( GUILayout.Button( "Host online", GUILayout.Width( 100f ), GUILayout.MinHeight(minButtonHeight) ) )
+		{
+			Network.InitializeServer( clientSlots, port, useNat );
+			MasterServer.RegisterHost(registeredGameType, registeredGameName, registeredGameComment);
+
+//			You can call RegisterHost more than once while a server is running 
+//			to update the information stored on the Master Server. For example, 
+//			if the server changes to a new level, you might call RegisterHost 
+//			again to update the lobby.
+		}
+		GUI.enabled = true;
 
 		GUILayout.BeginArea(new Rect(Screen.width * 0.5f, 0, Screen.width * 0.5f, Screen.height));
 		GUILayout.Label("Servers");
@@ -114,5 +176,87 @@ public class ConnectToGame : MonoBehaviour
 	{
 		Debug.Log( "Server initialized" );
 		NetworkLevelLoader.Instance.LoadLevel( Scenes.unityNetworkGameRoom );
+	}
+
+	void TestConnection() {
+		// Start/Poll the connection test, report the results in a label and 
+		// react to the results accordingly
+		connectionTestResult = Network.TestConnection();
+		switch (connectionTestResult) {
+		case ConnectionTesterStatus.Error: 
+			testMessage = "Problem determining NAT capabilities";
+			doneTesting = true;
+			break;
+			
+		case ConnectionTesterStatus.Undetermined: 
+			testMessage = "Undetermined NAT capabilities";
+			doneTesting = false;
+			break;
+			
+		case ConnectionTesterStatus.PublicIPIsConnectable:
+			testMessage = "Directly connectable public IP address.";
+			useNat = false;
+			doneTesting = true;
+			break;
+			
+			// This case is a bit special as we now need to check if we can 
+			// circumvent the blocking by using NAT punchthrough
+		case ConnectionTesterStatus.PublicIPPortBlocked:
+			testMessage = "Non-connectable public IP address (port " + serverPort +" blocked), running a server is impossible.";
+			useNat = false;
+			// If no NAT punchthrough test has been performed on this public 
+			// IP, force a test
+			if (!probingPublicIP) {
+				connectionTestResult = Network.TestConnectionNAT();
+				probingPublicIP = true;
+				testStatus = "Testing if blocked public IP can be circumvented";
+				timer = Time.time + 10;
+			}
+			// NAT punchthrough test was performed but we still get blocked
+			else if (Time.time > timer) {
+				probingPublicIP = false; 		// reset
+				useNat = true;
+				doneTesting = true;
+			}
+			break;
+		case ConnectionTesterStatus.PublicIPNoServerStarted:
+			testMessage = "Public IP address but server not initialized,\n"+	"it must be started to check server accessibility. Restart\n"+ "connection test when ready.";
+			break;
+			
+		case ConnectionTesterStatus.LimitedNATPunchthroughPortRestricted:
+			testMessage = "Limited NAT punchthrough capabilities. Cannot\n"+ "connect to all types of NAT servers. Running a server\n"+ "is ill advised as not everyone can connect.";
+			useNat = true;
+			doneTesting = true;
+			break;
+			
+		case ConnectionTesterStatus.LimitedNATPunchthroughSymmetric:
+			testMessage = "Limited NAT punchthrough capabilities. Cannot\n"+ "connect to all types of NAT servers. Running a server\n"+ "is ill advised as not everyone can connect.";
+			useNat = true;
+			doneTesting = true;
+			break;
+
+
+		case ConnectionTesterStatus.NATpunchthroughAddressRestrictedCone:
+			testMessage = "NAT punchthrough capable. Can connect to all\n"+ "servers and receive connections from all clients. Enabling\n"+ "NAT punchthrough functionality.";
+			useNat = true;
+			doneTesting = true;
+			break;
+		case ConnectionTesterStatus.NATpunchthroughFullCone:
+			testMessage = "NAT punchthrough capable. Can connect to all\n"+ "servers and receive connections from all clients. Enabling\n"+ "NAT punchthrough functionality.";
+			useNat = true;
+			doneTesting = true;
+			break;
+			
+		default: 
+			testMessage = "Error in test routine, got " + connectionTestResult;
+			break;
+		}
+		if (doneTesting) {
+			if (useNat)
+				shouldEnableNatMessage = "When starting a server the NAT\n"+ "punchthrough feature should be enabled (useNat parameter)";
+			else
+				shouldEnableNatMessage = "NAT punchthrough not needed";
+			testStatus = "Done testing";
+		}
 	}
 }
