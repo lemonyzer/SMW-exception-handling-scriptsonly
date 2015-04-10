ff92273e416a43a199c5f72e1248f910e655365d
Lemon Nyzer <lemonyzer@gmail.com>
2015-04-10 18:28:41 +0200

    "SuperMarioWars 2015.04.10_3 to 2015.04.07_5 Changes_ONLY.rar"

---
+++ Changes SuperMarioWars 2015.04.07_5 to 2015.04.10_3/Assets/ChangeColorBtnScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColorBtnScript : MonoBehaviour {
+
+	GameObject[] sprites;
+
+	void OnGUI()
+	{
+		if (GUILayout.Button ("Change Color"))
+		{
+			sprites = GameObject.FindGameObjectsWithTag("Player");
+
+			foreach(GameObject go in sprites)
+			{
+				ColorScript script = go.GetComponent<ColorScript>();
+				script.EditSpriteRendererSprite(go.GetComponent<SpriteRenderer>(), script.colorId);
+				script.colorId++;
+				script.colorId = script.colorId % 4;
+			}
+		}
+	}
+}
